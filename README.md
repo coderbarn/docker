9089db54f13a124a0598ac76ee315a85ff994f1a
coderbarn <coderbarn@users.noreply.github.com>
2022-03-06 14:36:59 -0500

    Still some updates

--- README.md
+++ README.md
@@ -4,9 +4,11 @@
 2. to ssh into container
 3. docker exec -it <name of the conatiner> /bin/bash
 4. docker exec -it compassionate_robinson /bin/bash
+5. An alternate sudo docker attach compassionate_robinson
 
 - docker images
 - docker run python-docker
 - docker run --publish 9880:80 python-docker
+- docker run --publish 9880:80 --name mydockertest -d python-docker
 - docker ps
 - docker rmi 783234ab3
--- sampleDocker/app.py
+++ sampleDocker/app.py
@@ -4,14 +4,14 @@ import os
 import socket
 
 # Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+# redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     try:
-        visits = redis.incr("counter")
+        visits = 1 #redis.incr("counter")
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
@@ -21,4 +21,4 @@ def hello():
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=9880)
+    app.run(host='0.0.0.0', port=80)
