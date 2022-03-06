ac33dc90914ddf4bc5a2b5717aad499a1601af48
coderbarn <coderbarn@users.noreply.github.com>
2022-03-06 15:12:55 -0500

    more updates

--- README.md
+++ README.md
@@ -6,11 +6,13 @@
 4. docker exec -it compassionate_robinson /bin/bash
 5. An alternate sudo docker attach compassionate_robinson
 
-- docker images
+- docker build . -t flasktestapp 
 - docker run python-docker
 - docker run --publish 9880:80 python-docker
 - docker run --publish 9880:80 --name mydockertest -d python-docker
 - docker ps
 - docker ps -a // Lists all containers
+- docker images
 - docker rmi 783234ab3
+- docker container ls -a // view all containers
 - docker container prune // removes all stopped containers
--- sampleDocker/app.py
+++ sampleDocker/app.py
@@ -11,14 +11,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     try:
-        visits = 1 #redis.incr("counter")
-    except RedisError:
+       visits = 1 #redis.incr("counter")
+    #except RedisError:
+    except:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    #return "hello world!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
